b36d94ec2fa90f69322f003d5e126e8040699f92
Ali Raza <142317127+Aalleyy@users.noreply.github.com>
2023-11-02 20:02:43 +0500

    added DList_template.cpp

---
+++ src/DList_template.cpp
@@ -0,0 +1,10 @@
+#include "Dlist.cpp"
+#include <string>
+
+template class DList<int>;
+template class DList<unsigned int>;
+template class DList<long>;
+template class DList<float>;
+template class DList<double>;
+template class DList<char>;
+template class DList<string>;
